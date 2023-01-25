c205e45f06aeb204b6ffd0b1e226dfba82b62323
deiseguth <deiiseguth@gmail.com>
2023-01-24 21:15:21 -0300

    1

---
+++ ConceitosBoasPraticas/src/ConceitosBoasPraticas.java
@@ -0,0 +1,28 @@
+public class ConceitosBoasPraticas {
+    /*
+     * Assinatura é a forma de identificar unicamente o método 
+     * Ass = nome + parâmetros
+     * 
+     * calcularTotalVenda (double precoItem1, double precoItem2, double precoItem3)
+     */
+    
+     /*
+      * Passagem de parâmtros:
+            Por valor:
+            int i = 10;
+            public void fazerAlgo(int i){
+                i = i + 10;
+                System.out.println("Valor de i dentro: " + i);
+            } System.out.println("Valor de i fora: " + i);
+      */
+
+      /*
+       * Boas práticas:
+       * Usar verbo + substantivo
+       *    Ex: vertificarSaldo ();
+       * Evitar artigos e preposições
+       * Métodos dewvem possuir entre 80 e 120 linhas
+       * Evitar listas de parâmetros longas
+       * 
+       */
+}
---
+++ ConceituacaoMetodos/src/ConceitoDefinicao.java
@@ -0,0 +1,30 @@
+public class ConceitoDefinicao {
+    // Todo método deve estar dentro de uma classe
+
+    /*
+    <?visibilidade?><?tipo?><?modificador?>retorno nome(<?parâmetros?>)<?exceções?>corpo
+
+     * Visibilidade: "public", "protected" ou "private". (mais usado)
+     * Tipo: concreto ou abstrato. 
+     * Modificador: "static" ou final. 
+     * Retorno: tipo de dado ou "void". (mais usado)
+     * Nome: nome que é fornecido ao método. (mais usado)
+     * Parâmetros: parâmetros que pode lançar. (mais usado)
+     * Exceções: exceções que pode lançar. 
+     * Corpo: código que possui ou vazio. (mais usado)
+     */
+}
+
+// public static Retorno Nome (parâmetro){}
+
+/*
+* Chamando por meio da classe:
+* nome_da_classe.nome_do_metodo();
+* Math.random(); ou Math.sqrt(4);
+* 
+
+* Chamando por meio do objeto:
+* nome_do_objeto.nome_do_metodo();
+* usuario.getEmail(); ou usuario.alterarEndereco(enderco);
+
+*/
