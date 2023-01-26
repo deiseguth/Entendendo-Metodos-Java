0acd3aeb027843355ed832e1fcf82cf904438439
deiseguth <deiiseguth@gmail.com>
2023-01-26 15:02:24 -0300

    2

---
+++ ExerciciosSobrecarga/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("\nExercício sobre Quadriláteros");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+
+    }
+}
---
+++ Sobrecargas/src/Sobrecarga.java
@@ -0,0 +1,24 @@
+public class Sobrecarga {
+    
+    // É a capacidade de definir métodos para diferentes contextos, mas preservando seu nome. Mantemos o nome e mudamos a lista de parâmetros. 
+
+    /*Quanto mais complexa a aplicação mais complexas ficam as sobrecargas. 
+
+    Alterar a assinatura do método:
+    Ass = nome + parâmetros
+    converterParaInteiro (float f);
+    converterParaInteiro(double d);
+    converterParaInteiro(float f, RoundType rd);
+    converterParaInteiro(double d, RoundType rd);
+    converterParaInteiro(String s, RoundType rd);
+    converterParaInteiro(RoundType rd, String s);
+
+    converterParaInteiro();
+
+
+    
+    Sobrecarga x sobrescrita
+
+    A sobrecarga precisa ter a herança (orientação a  objeto). Ambas atuam sobre o metodo, mas atuam de forma diferente. 
+    */
+}
